78e388495e4f3d89c6ff3208e098ac8b1fd90a46
Pere <peremontpeo@gmail.com>
2018-11-19 11:22:28 +0100

    buscador mes frequent arreglat

--- main.cc
+++ main.cc
@@ -1,27 +1,37 @@
-#include "word_toolkit.hpp"
+#include "word_toolkit.cpp"
 #include <iostream>
 
 int main()
 {
-    string s = "hola";
+    string s = "HOLA";
     bool result = word_toolkit::es_canonic(s);
     
     if (result) {
-        std::cout << "hola es canonic";
+        std::cout << "HOLA es canonic"  << endl;
     }
     else {
-        std::cout << "hola no es canonic";
+        std::cout << "HOLA no es canonic" << endl;
     }
-    s = "ahlo";
+    s = "AHLO";
     result = word_toolkit::es_canonic(s);
     
     if (result) {
-        cout << "ahlo es canonic";
+        cout << "AHLO es canonic" << endl;
     }
     else {
-        cout << "ahlo no es canonic";
+        cout << "AHLO no es canonic"  << endl;
     }
-    
+
+    cout << "anagrama canonic de HOLA es ";
+    s = "HOLA";
+    cout << word_toolkit::anagrama_canonic(s);
+    cout << endl;
+
+    initializer_list<string> paraules = { "HOLA", "ADEU", "JARDINER", "CLASSE", "TAULA", "PARAULA", "ESTUDI"};
+    string exclosos = "DA";
+    cout << "mes frequent es ";
+    cout << word_toolkit::mes_frequent(exclosos,paraules);
+    cout << endl;
 
     return 0;
 }
--- word_toolkit.cpp
+++ word_toolkit.cpp
@@ -1,47 +1,69 @@
 #include "word_toolkit.hpp"
 #include <algorithm>
-#define ASCII = 256
-// TODO: ens cal tot l'ASCII? Funció helper per transformar nomes 27 lletres
+#include <vector>
 
-//cost lineal
-bool word_toolkit::es_canonic(const string& s) throw() {
-    bool ho_es = true;
-    char car = s[0];
-    if (s.size() != 0) {
-        for (int i = 1; i < s.size() && ho_es; i++) {
-            if (s[i] > car) ho_es = false;
-            else car = s[i];
-        }
-    }
-    return ho_es;
+ bool word_toolkit::es_canonic(const string& s) throw(){
+  /*PRE: Cert
+	POST: Retorna cert si i només si les lletres de l'string s estan en 
+ 	ordre lexicogràfic ascendent. */
+ 	//Cost: lineal O(n)
+	bool t = true;
+	uint i = 1;
+	while(s.size() > i && t){
+		if(s[i] < s[i-1]) t = false;
+		else i++;
+	}
+	return t;
 }
 
-string word_toolkit::anagrama_canonic(const string& s) throw() {
-    return sort(s.begin(), s.end());
+string word_toolkit::anagrama_canonic(const string& s) throw(){
+  /*PRE: Cert
+	POST: Retorna l'anagrama canònic de l'string s.
+	Cost: quasi lineal O(n*log n) */
+	string anagrama = s;
+	if (!es_canonic(anagrama)){
+		sort(anagrama.begin(), anagrama.end());
+	}
+	return anagrama;
 }
 
-char word_toolkit::mes_frequent(const string& excl, const list<string>& L) throw() {
-    int comptador[ASCII] = {0};
-    list<string>::iterator it;
-    string str;
+char word_toolkit::mes_frequent(const string& excl, const list<string>& L) throw(){
+  /*PRE: L es una llista no buida.
+	POST: Retorna el caràcter que no apareix a l'string excl i és el més
+    freqüent en la llista de paraules L, sent L una llista no buida
+    de paraules formades exclusivament amb lletres majúscules de
+    la 'A' a la 'Z' (excloses la 'Ñ', 'Ç', majúscules accentuades, ...).
+    En cas d'empat, es retornaria el caràcter alfabèticament menor.
+    Si l'string excl inclou totes les lletres de la 'A' a la 'Z' es 
+    retorna el caràcter '\0', és a dir, el caràcter de codi ASCII 0.
+	Cost: lineal respecte al numero de caracters de string de la llista O(n) */
+	char resposta;
+	vector<int> contadors(26,0);
+	if(L.size() > 0){
+		for(list<string>::const_iterator it = L.begin(); it!=L.end(); ++it){
+			string paraula = *it;
+			for(uint j = 0; j<paraula.size(); j++){
+				int p = int(paraula[j]);
+				p = p-'A';
+				contadors[p]++;
+			}
+		}
+	}
+	for(uint i = 0; i<excl.size(); i++){
+		int p = int(excl[i]);
+		p = p-'A';
+		contadors[p] = -1;
+	}
+    int posicio = -1;
     int max = 0;
-    char resultat;
-    for (it = L.begin(); i != L.end(); it++) {
-        str = *it;
-        for (int i = 0; i < str.length(); i++) {
-            comptador[str[i]]++;
-            for (int j = 0; j < excl.length(); j++) {
-                if(str[i] == excl[j]) comptador[str[i]] = 0;
-            }
+    for(uint j = 0; j<contadors.size(); j++){
+    	if(max < contadors[j]) {
+            max = contadors[j];
+            posicio = j;
         }
     }
-    for(int i = ASCII; i < 0; i--) {
-        if (max < comptador[i]) {
-            max = comptador[i];
-            result = comptador[i];
-        }
-    }
-    //necessito donar-li una volta més a aquesta funcio...
-    return result;
+    if(posicio == -1) resposta = '\0';
+    else resposta = 'A' + posicio;
+    return resposta;
 
 }
