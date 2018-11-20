95003054f68f692b7722e425469163730d58c052
Pere <peremontpeo@gmail.com>
2018-11-20 15:16:31 +0100

    joc de proves word_toolkit

--- main.cc
+++ main.cc
@@ -1,37 +1,74 @@
-#include "word_toolkit.cpp"
 #include <iostream>
+#include <sstream>
+#include <list>
+#include "word_toolkit.cpp"
+using namespace std;
 
 int main()
 {
-    string s = "HOLA";
-    bool result = word_toolkit::es_canonic(s);
-    
-    if (result) {
-        std::cout << "HOLA es canonic"  << endl;
-    }
-    else {
-        std::cout << "HOLA no es canonic" << endl;
-    }
-    s = "AHLO";
-    result = word_toolkit::es_canonic(s);
-    
-    if (result) {
-        cout << "AHLO es canonic" << endl;
-    }
-    else {
-        cout << "AHLO no es canonic"  << endl;
-    }
+    string linea, comanda, word, excl;
+    while (getline(cin, linea))
+    {
+        list<string> paraules;
 
-    cout << "anagrama canonic de HOLA es ";
-    s = "HOLA";
-    cout << word_toolkit::anagrama_canonic(s);
-    cout << endl;
+        // Obtenim elements de excl
+        istringstream ss1(linea);
+        ss1 >> excl;
 
-    initializer_list<string> paraules = { "HOLA", "ADEU", "JARDINER", "CLASSE", "TAULA", "PARAULA", "ESTUDI"};
-    string exclosos = "DA";
-    cout << "mes frequent es ";
-    cout << word_toolkit::mes_frequent(exclosos,paraules);
-    cout << endl;
+        // Obtenim elements de la llista
+        while (ss1 >> word)
+        {
+            paraules.push_back(word);
+        }
 
-    return 0;
+        // Processem comandes
+        while (getline(cin, linea) and linea != "----------")
+        {
+            cout << linea << ": ";
+            istringstream ss2(linea);
+            ss2 >> comanda;
+
+            if (comanda == "insereix")
+            {
+                ss2 >> word;
+                paraules.push_back(word);
+                cout << paraules.size();
+            }
+            else if (comanda == "elimina")
+            {
+                ss2 >> word;
+                paraules.remove(word);
+                cout << paraules.size();
+            }
+            else if (comanda == "quants")
+            {
+                cout << paraules.size();
+            }
+            else if (comanda == "print")
+            {
+                for (auto w : paraules)
+                {
+                    cout << w << ' ';
+                }
+            }
+            else if (comanda == "es_canonic")
+            {
+                ss2 >> word;
+                cout << word_toolkit::es_canonic(word);
+            }
+            else if (comanda == "anagrama_canonic")
+            {
+                ss2 >> word;
+                cout << word_toolkit::anagrama_canonic(word);
+            }
+            else if (comanda == "mes_frequent")
+            {
+                //cout << "mes frequent es ";
+                cout << word_toolkit::mes_frequent(excl, paraules);
+            }
+            cout << endl;
+        }
+        if (linea == "----------")
+            cout << linea << endl;
+    }
 }
