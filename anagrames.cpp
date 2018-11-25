3aebeead252e2f544338fe45767186a6c5bdb762
Pere <peremontpeo@gmail.com>
2018-11-25 17:20:43 +0100

    declaracio funcions

--- anagrames.cpp
+++ anagrames.cpp
@@ -1,1 +1,25 @@
 #include "anagrames.hpp"
+
+anagrames::anagrames() throw(error) {
+
+}
+
+anagrames::anagrames(const anagrames& A) throw(error) {
+
+}
+
+anagrames::operator=(const anagrames& A) throw(error) {
+
+}
+
+anagrames::~anagrames() throw() {
+
+}
+
+void anagrames::insereix(const string& p) throw(error) {
+
+}
+
+void anagrames::mateix_anagrama_canonic(const string& a, list<string>& L) const throw(error) {
+    
+}
--- diccionari.cpp
+++ diccionari.cpp
@@ -1,1 +1,37 @@
 #include "diccionari.hpp"
+
+diccionari::diccionari() throw(error) {
+
+}
+
+diccionari::diccionari(const diccionari& D) throw(error) {
+
+}
+
+diccionari::operator=(const diccionari& D) throw(error) {
+
+}
+
+diccionari::~diccionari() throw() {
+
+}
+
+void diccionari::insereix(const string& p) throw(error) {
+
+}
+
+string diccionari::prefix(const string& p) const throw(error) {
+
+}
+
+void diccionari::satisfan_patro(const vector<string>& q, list<string>& L) const throw(error) {
+
+}
+
+void diccionari::llista_paraules(nat k, list<string>& L) const throw(error) {
+
+}
+
+nat diccionari::num_pal() const throw() {
+    
+}
