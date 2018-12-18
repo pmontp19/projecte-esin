422ec7bbcc78fc0b50041b6e9914bcc53b52e177
Pere <peremontpeo@gmail.com>
2018-12-18 11:58:22 +0100

    main_anagrames

--- main_anagrames.cpp
+++ main_anagrames.cpp
@@ -20,46 +20,41 @@ int main()
     a.insereix(par4);
     a.insereix(par5);
     anagrames b(a);
-    cout << "numero paraules " << a.num_pal() << endl;
+    cout << "anagrames a numero paraules " << a.num_pal() << endl;
     list<string> l;
     a.mateix_anagrama_canonic("COORR", l);
     while (!l.empty()) {
         cout << l.front() << endl;
         l.pop_front();
     }
-    cout << "numero paraules " << b.num_pal() << endl;
+    cout << "anagrames b numero paraules " << b.num_pal() << endl;
     list<string> l2;
     b.mateix_anagrama_canonic("COORR", l2);
     while (!l2.empty()) {
         cout << l2.front() << endl;
-        l.pop_front();
+        l2.pop_front();
     }
 
-   /* diccionari a;
-    string par1 = "CORRO";
-    string par2 = "CORRO";
-    string par3 = "CORRO";
-    string par4 = "CACAA";
-    string par5 = "CZCAA";
-    a.insereix(par1);
-    a.insereix(par2);
-    a.insereix(par3);
-    a.insereix(par4);
-    a.insereix(par5);
-    diccionari b(a);
-    cout << "numero paraules " << a.num_pal() << endl;
-    cout << "numero paraules " << b.num_pal() << endl;
-    list<string> l;
-    a.llista_paraules(0, l);
-    while (!l.empty()) {
-        cout << l.front() << endl;
-        l.pop_front();
+    diccionari d;
+    d.insereix(par1);
+    d.insereix(par2);
+    d.insereix(par3);
+    d.insereix(par4);
+    d.insereix(par5);
+    diccionari e(d);
+    cout << "diccionari d numero paraules " << d.num_pal() << endl;
+    list<string> l3;
+    d.llista_paraules(0, l3);
+    while (!l3.empty()) {
+        cout << l3.front() << endl;
+        l3.pop_front();
     }
-    b.llista_paraules(0, l);
-    while (!l.empty()) {
-        cout << l.front() << endl;
-        l.pop_front();
+    cout << "diccionari e numero paraules " << e.num_pal() << endl;
+    e.llista_paraules(0, l3);
+    while (!l3.empty()) {
+        cout << l3.front() << endl;
+        l3.pop_front();
     }
-*/
+
     return 0;
 }
