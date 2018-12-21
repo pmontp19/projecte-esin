3f67c7c38ce007444144550fa8785b57e1af9239
Pere <peremontpeo@gmail.com>
2018-12-21 11:11:31 +0100

    jp_privat afegit

--- iter_subset.cpp
+++ iter_subset.cpp
@@ -4,7 +4,7 @@ iter_subset::iter_subset(nat n, nat k) throw(error): _n(n), _k(k), _actual(k) {
   /** 
    * Pre:  k <= n.
    * Post: Construeix un iterador sobre els subconjunts de k elements 
-           de {1, ..., n}; si k > n no hi ha res a recórrer. 
+   *       de {1, ..., n}; si k > n no hi ha res a recórrer. 
    * Cost: O(k) sent k el tamany del subset.
   */
   _final = false;
@@ -53,9 +53,9 @@ bool iter_subset::end() const throw() {
   /** 
    * Pre:  Cert.
    * Post: Retorna cert si l'iterador ja ha visitat tots els subconjunts
-           de k elements presos d'entre n; o dit d'una altra forma, retorna
-           cert quan l'iterador apunta a un subconjunt sentinella fictici 
-           que queda a continuació de l'últim subconjunt vàlid.
+   *       de k elements presos d'entre n; o dit d'una altra forma, retorna
+   *       cert quan l'iterador apunta a un subconjunt sentinella fictici 
+   *       que queda a continuació de l'últim subconjunt vàlid.
    * Cost: Constant O(1).
   */
   return _final;
@@ -64,7 +64,7 @@ subset iter_subset::operator*() const throw(error) {
   /** 
    * Pre:  Cert.
    * Post: Operador de desreferència. Retorna el subconjunt apuntat per
-           l'iterador; llança un error si l'iterador apunta al sentinella.
+   *       l'iterador; llança un error si l'iterador apunta al sentinella.
    * Cost: Constant O(1).
   */
   if (_final) throw error(IterSubsetIncorr);
@@ -75,8 +75,8 @@ iter_subset& iter_subset::operator++() throw() {
   /** 
    * Pre:  Cert.
    * Post: Operador de preincrement. 
-           Avança l'iterador al següent subconjunt en la seqüència i el retorna; 
-           no es produeix l'avançament si l'iterador ja apuntava al sentinella.
+   *       Avança l'iterador al següent subconjunt en la seqüència i el retorna; 
+   *       no es produeix l'avançament si l'iterador ja apuntava al sentinella.
    * Cost: O(k) sent k el tamany del subset.
   */
   bool acabat = false, modificat = false;
@@ -109,8 +109,8 @@ iter_subset iter_subset::operator++(int) throw() {
   /** 
    * Pre:  Cert.
    * Post: Operador de postincrement. 
-           Avança l'iterador al següent subconjunt en la seqüència i retorna el seu valor
-           previ; no es produeix l'avançament si l'iterador ja apuntava al sentinella. 
+   *       Avança l'iterador al següent subconjunt en la seqüència i retorna el seu valor
+   *       previ; no es produeix l'avançament si l'iterador ja apuntava al sentinella. 
    * Cost: O(k) sent k el tamany del subset.
   */
   return ++*this;
