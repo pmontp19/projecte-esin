320fa93859d41f8591a1a27c4a7455ea45d5a4f8
Pere <peremontpeo@gmail.com>
2019-01-03 11:03:17 +0100

    canvis esteve

--- script_entrega.sh
+++ script_entrega.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-tar zcvf projecte-esin-victor-pere.tar \
-anagrames.cpp anagrames.hpp anagrames.rep \
-diccionari.cpp diccionari.hpp diccionari.rep \
-iter_subset.cpp iter_subset.hpp iter_subset.rep \
-obte_paraules.cpp obte_paraules.hpp \
-word_toolkit.cpp word_toolkit.hpp \
-documentacio_projecte.pdf
+tar cvzf 02.tgz word_toolkit.cpp iter_subset.rep iter_subset.cpp \
+diccionari.rep diccionari.cpp anagrames.rep anagrames.cpp \
+obte_paraules.cpp
